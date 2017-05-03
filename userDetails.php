a56062205ff09c678b085fbc7cb86dbf651b75f8
byarranton <byarranton@hotmail.com>
2017-05-04 07:15:13 +1000

    Two newly created files for displaying user's details
    "userDetails.php" displays the user's details (username, phone number, etc) and allows the user to edit any details.
    "changeDetails.php" is a server-side-only script. If the user edits any of their details on the userDetails.php page, the changes are
    sent to the changeDetails.php, which updates the database.
    Note that both files require the "classes.php" file (uploaded moments ago).

--- userDetails.php
+++ userDetails.php
@@ -1,9 +1,8 @@
 
 <?php
 
-session_start();
-//bring in config file to gain access to support functions
-require 'phpfunctions\config.php';
+require_once 'classes.php';
+require_once 'header.php';
 $current = new currentUser();	//the user that is currently logged in (if any)
 if (!$current->username())
 {
@@ -14,14 +13,26 @@ if (!$current->username())
 }
 ?>
 
-<?php include 'phpfunctions\header-standard.php';?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Job Application Tracker</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+
+<?php //include('header.php');?>
+
 <div class="main">
 	<div class="container">
 		<div class="Jumbotron">
           <h1>My Details</h1>
           <h2>Review or edit your details:</h2>
         </div>
-		<?php
+		<?php 
 		if(isset($_SESSION['message'])){
 			echo "<p>".$_SESSION['message']."</p>";
 			unset($_SESSION['message']);
@@ -31,59 +42,74 @@ if (!$current->username())
 			<div class="form-group">
 				<label class="col-sm-2">Username:</label>
 				<label class="col-sm-2"><?php echo $current->username();?></label>
-				<span class="col-sm-8"><button onClick="toggleVisibility('editusername')">edit</button></span>
+				<span class="col-sm-8"><button class="toggle">edit</button></span>
 			</div>
-			<form class="form-horizontal" method="POST" action="changeDetails.php">
-			<div class="form-group" id="editusername" style="display: none">
-				<label class="control-label col-sm-2" for="newusername">New username:</label>
-				<div class="col-sm-2">
-					<input type="text" id="newusername" name="newusername" class="content" onblur="checkUsername(this.value)"/><br>
+			
+			<div class="editfield">
+				<form class="form-horizontal" method="POST" action="changeDetails.php">
+				<div class="form-group" id="editusername">
+					<label class="control-label col-sm-2" for="newusername">New username:</label> 
+					<div class="col-sm-2">  
+						<input type="text" id="newusername" name="newusername" class="content" onblur="checkUsername(this.value)"/><br>
+					</div>
+					<span class="col-sm-8"><button>sumbit change</button></span>
 				</div>
-				<span class="col-sm-8"><button>sumbit change</button></span>
+				</form>
 			</div>
-			</form>
+			
 			<div class="form-group">
 				<label class="col-sm-2">Password:</label>
 				<label class="col-sm-2">hidden</label>
-				<span class="col-sm-8"><button onClick="toggleVisibility('editpassword')">edit</button></span>
+				<span class="col-sm-8"><button class="toggle">edit</button></span>
 			</div>
-			<form class="form-horizontal" method="POST" action="changeDetails.php">
-			<div class="form-group" id="editpassword" style="display: none">
-				<label class="control-label col-sm-2" for="newpassword">New password:</label>
-				<div class="col-sm-2">
-					<input type="password" id="newpassword" name="newpassword" class="content" onblur="checkUsername(this.value)"/><br>
+			
+			<div class="editfield">
+				<form class="form-horizontal" method="POST" action="changeDetails.php">
+				<div class="form-group" id="editpassword">
+					<label class="control-label col-sm-2" for="newpassword">New password:</label> 
+					<div class="col-sm-2">  
+						<input type="password" id="newpassword" name="newpassword" class="content" onblur="checkUsername(this.value)"/><br>
+					</div>
+					<span class="col-sm-8"><button>sumbit change</button></span>
 				</div>
-				<span class="col-sm-8"><button>sumbit change</button></span>
+				</form>
 			</div>
-			</form>
+			
 			<div class="form-group">
 				<label class="col-sm-2">Email:</label>
 				<label class="col-sm-2"><?php echo $current->email();?></label>
-				<span class="col-sm-8"><button onClick="toggleVisibility('editemail')">edit</button></span>
+				<span class="col-sm-8"><button class="toggle">edit</button></span>
 			</div>
-			<form class="form-horizontal" method="POST" action="changeDetails.php">
-			<div class="form-group" id="editemail" style="display: none">
-				<label class="control-label col-sm-2" for="newemail">New email:</label>
-				<div class="col-sm-2">
-					<input type="text" id="newemail" name="newemail" class="content" onblur="checkUsername(this.value)"/><br>
+			
+			<div class="editfield">
+				<form class="form-horizontal" method="POST" action="changeDetails.php">
+				<div class="form-group" id="editemail">
+					<label class="control-label col-sm-2" for="newemail">New email:</label> 
+					<div class="col-sm-2">  
+						<input type="text" id="newemail" name="newemail" class="content" onblur="checkUsername(this.value)"/><br>
+					</div>
+					<span class="col-sm-8"><button>sumbit change</button></span>
 				</div>
-				<span class="col-sm-8"><button>sumbit change</button></span>
+				</form>
 			</div>
-			</form>
+			
 			<div class="form-group">
 				<label class="col-sm-2">Phone:</label>
 				<label class="col-sm-2"><?php echo $current->phone();?></label>
-				<span class="col-sm-8"><button onClick="toggleVisibility('editphone')">edit</button></span>
+				<span class="col-sm-8"><button class="toggle">edit</button></span>
 			</div>
-			<form class="form-horizontal" method="POST" action="changeDetails.php">
-			<div class="form-group" id="editphone" style="display: none">
-				<label class="control-label col-sm-2" for="newphone">New phone:</label>
-				<div class="col-sm-2">
-					<input type="text" id="newphone" name="newphone" class="content" onblur="checkUsername(this.value)"/><br>
+			
+			<div class="editfield">
+				<form class="form-horizontal" method="POST" action="changeDetails.php">
+				<div class="form-group" id="editphone">
+					<label class="control-label col-sm-2" for="newphone">New phone:</label> 
+					<div class="col-sm-2">  
+						<input type="text" id="newphone" name="newphone" class="content" onblur="checkUsername(this.value)"/><br>
+					</div>
+					<span class="col-sm-8"><button>sumbit change</button></span>
 				</div>
-				<span class="col-sm-8"><button>sumbit change</button></span>
+				</form>
 			</div>
-			</form>
 		</div>
 	</div>
 </div>
@@ -93,11 +119,19 @@ if (!$current->username())
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-<?php echo "username is ".$username?></br>
-<?php echo "username is ".$current->username()?></br>
-<?php echo "password is ".$current->password()?></br>
-<?php echo "email is ".$current->email()?></br>
-<?php echo "phone is ".$current->phone()?></br>
+    
+    <script>
+    //This method sets the "editfield" elements to hidden upon page load.
+    //It binds the "edit" buttons so that when one is clicked it slides the corresponding
+    //edit form down.
+    $(document).ready(function(){
+        $(".editfield").hide();
+        $(".toggle").click(function(){
+            $(this).closest("div").next().slideToggle();
+            });
+        });
+    </script>
 
+<?php include 'sessionMessage.php'?>
 </body>
 </html>
