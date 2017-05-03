5523160b0f69ef63a7e80a316a67f7a0d43dd57b
byarranton <byarranton@hotmail.com>
2017-05-04 07:59:05 +1000

    Two newly created files for user login
    "login.php" presents a form allowing the user to log in.
    "checkLogin.php" receives the info from the form and checks the credentials against the database.

---
+++ checkLogin.php
@@ -0,0 +1,69 @@
+
+//<!-this page is called from login.php after the user submits their username
+//and password->
+
+//<!-first, get the variables that were submitted from the login.php form->
+<?php
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password1"];
+
+session_start();
+//<!-now call the database to check if the username and password match->
+//bring in the config file to get the database access credentials
+require 'config.php';
+require_once 'classes.php';
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	//die("Connection failed: " . $conn->connect_error);
+	$_SESSION['message'] = "Whoops, looks like something went wrong with the database, please try again";
+	header("Location: login.php");
+}
+
+//query the database
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
+
+//parse the query results
+if ($result->num_rows > 0)
+{
+	while ($row = $result->fetch_assoc())
+	{
+		if ($row['username'] == $username && password_verify($password, $row['password']))
+		{
+			//then the user credentials are correct
+			//$_SESSION['message'] = "Welcome back, ".$username;	//message to display in the userCalendar page			
+			//$_SESSION['message'] = "username, pword, email, phone, staysign =".$username.$row['password'].$row['email'].$row['phone'].$staySignedIn;	//message to display in the userCalendar page
+			//check if the user elected to "stay signed in"
+			if (isset($_REQUEST["staySignedIn"]))
+			{
+				$staySignedIn = 1;
+			} else {
+				$staySignedIn = 0;
+			}
+			//set the session and cookie details
+			$current = new currentUser();
+			$current->setAll($username, $row['password'], $row['email'],
+					$row['phone'], $staySignedIn);
+			/*
+			$current = new currentUser($username, $row['password'], $row['email'],
+					$row['phone'], $staySignedIn);
+			*/
+			header("Location: userCalendar2.php");	//redirect to calendar
+			exit;
+		} else {
+			//then the user credentials were incorrect
+			$_SESSION['message'] = "Whoops, username/password was incorrect, have another go...";
+			header("Location: login.php");
+			exit;
+		}
+	}
+} else {
+	$_SESSION['message'] = "Seems that username/password was incorrect, have another go...";
+	header("Location: login.php");
+}
+
+?>
---
+++ login.php
@@ -0,0 +1,107 @@
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Job Application Tracker</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="main">
+	<div class="container">
+		<div class="Jumbotron">
+          <h1>Login</h1>
+          <h2>get in here!</h2>
+        </div>
+		
+		<!--Main login form-->
+		<form class="form-horizontal" method="POST" action="checkLogin.php">
+			<?php 
+			if(isset($_SESSION['message'])){
+				echo "<p>".$_SESSION['message']."</p>";
+				unset($_SESSION['message']);
+			}
+			?>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="username">Username:</label> 
+				<div class="col-sm-10">  
+					<input type="text" id="username" name="username" class="content"/><br>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="password1">Password:</label>
+				<div class="col-sm-10">  
+					<input type="password" id="password1" name="password1" class="content" /><br>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<div class="checkbox">
+						<label><input type="checkbox" name="staySignedIn"> Keep me signed in</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">  
+					<button type="submit" class="btn btn-default">Sign In</button>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<p class="col-sm-offset-2 col-sm-10"><a href="javascript:forgotLogin()">Forgot your login?</a></p>
+			</div>
+			
+		</form>
+		
+		<!--Recover login details form-->
+		<form class="form-horizontal" id="forgotLogin" style="display: none" method="POST" action="forgotLogin.php">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Email:</label> 
+				<div class="col-sm-10">  
+					<input type="text" id="email" name="email" class="content"/><br>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="phone">Phone:</label>
+				<div class="col-sm-10">  
+					<input type="text" id="phone" name="phone" class="content" /><br>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">  
+					<button type="submit" class="btn btn-default">Retrieve login</button>
+				</div>
+			</div>
+			
+		</form>
+		
+		<div class="form-group">
+			<label class="control-label col-sm-2">Don't have an account?</label>
+			<div class="col-sm-10">  
+				<p><a href="register2.php">Create one</a></p>
+			</div>
+		</div>
+			
+	</div>
+</div>
+
+<script>
+function forgotLogin()
+{
+	//toggles the forgotten login form visibility
+	//document.getElementById("testlab").innerHTML = "it worked";
+	var x = document.getElementById("forgotLogin");
+	if (x.style.display === 'none')
+	{
+		x.style.display = 'block';
+	} else {
+		x.style.display = 'none';
+	}
+};
+</script>
+
+</body>
+</html>
